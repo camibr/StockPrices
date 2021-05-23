b70054c1d72dacfd9fddeb51b3c2897941d8fb82
camibr <63915384+camibr@users.noreply.github.com>
2021-05-23 16:37:52 +0200

    Update backtest.py

--- backtest.py
+++ backtest.py
@@ -5,8 +5,7 @@ from abc import ABCMeta, abstractmethod
 
 class Strategy:
 # take a list of candle bars as input (Open-High-Low-Close-Volume) and produce movement 
-# recommendations i.e. positions going long, hold or short {1, 0, -1} for Portfolio class to take
-# decision on the position """    
+# recommendations i.e. positions going long, hold or short {1, 0, -1} 
     
     @abstractmethod
     def generate_signs(self):
